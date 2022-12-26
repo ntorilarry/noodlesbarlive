092069f968061dbb496f593d5c84ab33e557653e
Larry Ntori <l.ntori@generisdevelopers.com>
2022-12-26 11:13:57 +0000

    email button added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import "../styles/home.css";
+import { BsChatRightTextFill } from "react-icons/bs";
 
 function Home() {
   return (
@@ -12,15 +13,15 @@ function Home() {
             <div className="px-4 py-24 mx-auto  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-64 h-[90vh] flex flex-col justify-center content-center">
               <div className="">
                 <div className="w-full max-w-xl xl:mb-0 xl:pr-16 xl:w-7/12">
-                  <h2 className="max-w-lg mb-6 text-center sm:text-left text-5xl font-semibold tracking-tight text-white md:text-5xl lg:text-6xl xl:text-7xl sm:leading-none bigsect underlined underlined--thin">
+                  <h2 className="max-w-lg mb-6 text-5xl font-semibold tracking-tight text-center text-white sm:text-left md:text-5xl lg:text-6xl xl:text-7xl sm:leading-none bigsect underlined underlined--thin">
                     Personal Chef Experience
                   </h2>
                   {/* <img
-                    className="line mb-6 mx-auto lg:mx-0"
+                    className="mx-auto mb-6 line lg:mx-0"
                     src="https://i.postimg.cc/ZRdtVH1K/title-line-2.png"
                     alt=""
                   /> */}
-                  <p className="max-w-xl mb-4 font-normal text-sm text-white md:text-sm lg:text-base text-center sm:text-left">
+                  <p className="max-w-xl mb-4 text-sm font-normal text-center text-white md:text-sm lg:text-base sm:text-left">
                     Accra traffic jam is ready, can you stop the one that’s
                     missing it stereo ? Explore our saucy or dry flavors ranging
                     from mid-to-hot, in doice or savory. Browse our menu
@@ -28,7 +29,7 @@ function Home() {
                   <div className="flex flex-wrap sm:flex-nowrap">
                     <a
                       href="https://wa.me/message/E3A3VLWVVBTSK1"
-                      className="relative inline-flex mx-auto sm:mx-0 items-center justify-center p-4 px-12 py-4 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-white rounded-full shadow-md group"
+                      className="relative inline-flex items-center justify-center p-4 px-12 py-4 mx-auto overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-white rounded-full shadow-md sm:mx-0 group"
                     >
                       <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-white group-hover:translate-x-0 ease">
                         <svg
@@ -39,14 +40,14 @@ function Home() {
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M14 5l7 7m0 0l-7 7m7-7H3"
                           ></path>
                         </svg>
                       </span>
-                      <span className="absolute flex items-center justify-center w-full h-full text-white  transition-all duration-300 transform group-hover:translate-x-full ease">
+                      <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
                         Whatsapp Us
                       </span>
                       <span className="relative invisible">Button Text</span>
@@ -56,6 +57,14 @@ function Home() {
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          <a href="mailto:add.atiemo@gmail.com">
+            <BsChatRightTextFill
+              size={50}
+              className="fixed text-white animate-bounce bottom-5 right-14"
+            />
+          </a>
         </div>
       </div>
     </div>
--- src/pages/meals.tsx
+++ src/pages/meals.tsx
@@ -13,198 +13,208 @@ import Markola from "../assets/markola.jpeg";
 import OlonkaBelly from "../assets/olonkabelly.jpeg";
 import Trinity from "../assets/trinity.jpeg";
 import Wokspicy from "../assets/wokspicy.jpeg";
+import Mealvideo from "../assets/meal.mp4";
 
 function Meals() {
   return (
     <div>
       <Navbar />
-      <section className="py-6 sm:py-12 bg-white text-white">
+      <section className="pb-6 text-white bg-white sm:pb-12">
+        <div className="w-full h-full">
+          <video
+            className="object-cover object-center w-full"
+            src={Mealvideo}
+            autoPlay
+            loop
+            controls
+          />
+        </div>
         <div className="container p-6 mx-auto space-y-8">
           <div className="space-y-2 text-center">
-            <h2 className="text-4xl text-gray-600 font-semibold">
-              Our delicious meals
+            <h2 className="my-2 text-3xl font-semibold text-gray-600 sm:text-4xl">
+              Our Delicious Meals
             </h2>
           </div>
           <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={AccraTraffic}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Accra Beef Traffic Jam
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 50
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={AfiTower}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Trinity Tasty Stir
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 40
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Agotime}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Akora Cucumber Wings
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 30
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Akora}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Biggest Akuapim flying Taste
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 45
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Akorfa}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Akorfa Spicy Veggies
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 50
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Asasewa}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Asesewa Market Signature Egg Loaf
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 30
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={BiggestStick}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Wok-Spicy Shrimp Stem
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 50
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={GoldCoast}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Gold Coast Fashion Stirring
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 35
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Legon}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Legon Confidence Corn Sauced
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 55
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Markola}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Hot Makola Soup Glaze
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 25
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Trinity}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Agotime Love 1831.ave
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 35
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={OlonkaBelly}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                Olonka Belly Pounced Sausages
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 30
                 </h3>
               </div>
             </article>
             <article className="flex flex-col bg-[#00a14b]">
               <img
                 alt=""
-                className="object-cover w-full h-52 bg-gray-500"
+                className="object-cover w-full bg-gray-500 h-52"
                 src={Wokspicy}
               />
 
               <div className="flex flex-col flex-1 p-6">
-                Convenire
+                AFI Chilies Swiss
                 <h3 className="flex-1 py-2 text-lg font-semibold leading-snug">
-                  Te nulla oportere reprimique his dolorum
+                  Starting price GHC 25
                 </h3>
               </div>
             </article>
