199c34b165d3bdf51c8aaf6ca05bc1bcb114f394
Larry Ntori <l.ntori@generisdevelopers.com>
2022-12-27 19:12:49 +0000

    seo added

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,39 @@ import About from "./pages/about";
 import Pagenotfound from "./pages/pagenotfound";
 import Blog from "./pages/blog";
 import Meals from "./pages/meals";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        {/* Main Routes */}
-        <Route path="/">
-          <Route index element={<Navigate replace to="home" />} />
-          <Route path="home" element={<Home />} />
-          <Route path="mission" element={<Mission />} />
-          <Route path="about-us" element={<About />} />
-          <Route path="meals" element={<Meals />} />
-          <Route path="blog" element={<Blog />} />
-        </Route>
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Noodlesbar</title>
+        <meta
+          name="description"
+          content="Accra traffic jam is ready, can you stop the one that’s
+          missing it stereo ? Explore our saucy or dry flavors ranging
+          from mid-to-hot, in doice or savory. Browse our menu"
+        />
+        <link rel="canonical" href="https://www.noodlesbar.live" />
+      </Helmet>
+      <div className="App">
+        <Routes>
+          {/* Main Routes */}
+          <Route path="/">
+            <Route index element={<Navigate replace to="home" />} />
+            <Route path="home" element={<Home />} />
+            <Route path="mission" element={<Mission />} />
+            <Route path="about-us" element={<About />} />
+            <Route path="meals" element={<Meals />} />
+            <Route path="blog" element={<Blog />} />
+          </Route>
 
-        {/* Not found */}
-        <Route path="*" element={<Pagenotfound />} />
-      </Routes>
-    </div>
+          {/* Not found */}
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      </div>
+    </HelmetProvider>
   );
 }
 
