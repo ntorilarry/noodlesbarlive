a62ba98c9ec9af8d2b6f2d09bfb35b4f49188d67
Larry Ntori <l.ntori@generisdevelopers.com>
2023-02-08 17:36:10 +0000

    blog updates

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -57,53 +57,17 @@ function Blog() {
                 resuscitate.
               </div>
               <p className="pb-6">
-                Addomprehh The term Addompreh from Akuapim, happiness, refer to
+                The term Addompreh from Akuapim, happiness, refer to
                 the enthusiasm for everything related to food. Think food think
                 Addompreh Food + mind + action = result
               </p>
 
               <p className="pb-6">
-                I am the very tradition of Morden cultural ego. I have Creative
+                I am the very tradition of modern cultural ego. I have Creative
                 vegetables black and white and I know the kings of Akuapeman,
                 And i quote God thereof.
               </p>
-              {/* <h2 className="mt-4 mb-4 text-2xl font-semibold text-gray-800">
-                Uneasy barton seeing remark happen his has
-              </h2>
-              <p className="pb-6">
-                Guest it he tears aware as. Make my no cold of need. He been
-                past in by my hard. Warmly thrown oh he common future. Otherwise
-                concealed favourite frankness on be at dashwoods defective at.
-                Sympathize interested simplicity at do projecting increasing
-                terminated. As edward settle limits at in.
-              </p>
-              <p className="pb-6">
-                Dashwood contempt on mr unlocked resolved provided of of.
-                Stanhill wondered it it welcomed oh. Hundred no prudent he
-                however smiling at an offence. If earnestly extremity he he
-                propriety something admitting convinced ye. Pleasant in to
-                although as if differed horrible. Mirth his quick its set front
-                enjoy hoped had there. Who connection imprudence middletons too
-                but increasing celebrated principles joy. Herself too improve
-                gay winding ask expense are compact. New all paid few hard pure
-                she.
-              </p>
-              <p className="pb-6">
-                Breakfast agreeable incommode departure it an. By ignorant at on
-                wondered relation. Enough at tastes really so cousin am of.
-                Extensive therefore supported by extremity of contented. Is
-                pursuit compact demesne invited elderly be. View him she roof
-                tell her case has sigh. Moreover is possible he admitted
-                sociable concerns. By in cold no less been sent hard hill.
-              </p>
-              <p className="pb-6">
-                Detract yet delight written farther his general. If in so bred
-                at dare rose lose good. Feel and make two real miss use easy.
-                Celebrated delightful an especially increasing instrument am.
-                Indulgence contrasted sufficient to unpleasant in in insensible
-                favourable. Latter remark hunted enough vulgar say man. Sitting
-                hearted on it without me.
-              </p> */}
+             
             </div>
       
           </div>
