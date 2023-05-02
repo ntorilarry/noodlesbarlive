2c1d16aa65aa7379f8439e85aaf42767ef7477f8
Larry Ntori <l.ntori@generisdevelopers.com>
2023-05-02 15:48:53 +0000

    animate addedd

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,6 @@ import "aos/dist/aos.css";
 import Footer from "../components/Footer";
 import Heropic from "../assets/heropic.png";
 import Mission from "../components/mission";
-import { Link } from "react-router-dom";
 import Features from "../components/Features";
 import Pattern from "../assets/bgpatttern.png";
 
@@ -30,10 +29,20 @@ function Home() {
             <div className="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-16">
               <div>
                 <div className="text-center lg:text-left">
-                  <h1 className="text-4xl font-bold leading-tight text-white sm:text-5xl lg:text-6xl font-ph">
+                  <h1
+                    data-aos="fade-right"
+                    data-aos-delay="1000"
+                    data-aos-once="true"
+                    className="text-4xl font-bold leading-tight text-white sm:text-5xl lg:text-6xl font-ph"
+                  >
                     Personal Chef Experience
                   </h1>
-                  <p className="mt-2 text-md text-white sm:mt-4">
+                  <p
+                    data-aos="fade-right"
+                    data-aos-delay="1200"
+                    data-aos-once="true"
+                    className="mt-2 text-md text-white sm:mt-4"
+                  >
                     Accra traffic jam is ready, can you stop the one that’s
                     missing it stereo ? Explore our saucy or dry flavors ranging
                     from mid-to-hot, in doice or savory. Browse our menu
@@ -41,6 +50,9 @@ function Home() {
 
                   <div className="flex items-center justify-center mt-4 space-x-5 lg:justify-start">
                     <a
+                      data-aos="fade-right"
+                      data-aos-delay="1300"
+                      data-aos-once="true"
                       href="/meals"
                       className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300"
                     >
@@ -50,7 +62,11 @@ function Home() {
                   </div>
                 </div>
               </div>
-              <div>
+              <div
+                data-aos="fade-left"
+                data-aos-delay="1400"
+                data-aos-once="true"
+              >
                 <img className="w-full m-auto" src={Heropic} alt="" />
               </div>
             </div>
