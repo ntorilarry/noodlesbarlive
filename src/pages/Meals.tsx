593947cb7a3d81e2fb7b90607f55fa7b8966984b
Larry Ntori <l.ntori@generisdevelopers.com>
2023-07-11 21:38:21 +0000

    noodlesbar

--- src/pages/Meals.tsx
+++ src/pages/Meals.tsx
@@ -290,8 +290,8 @@ export default function Meals() {
                   </span>
                 </div>
               </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
+              <h3 className="mt-4 text-lg text-gray-700">{product.name}</h3>
+              <p className="mt-1 text-sm font-medium text-gray-900">
                 {product.price}
               </p>
             </a>
